c0020f70c93d81f6a059bda2b3f90ea97bc18524
josephroby <50609673+josephroby@users.noreply.github.com>
2021-09-14 11:14:55 +0530

    Add files via upload

---
+++ EmployeeDetails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class EmployeeDetails
+    {
+
+
+        
+        public static string EmployeeId, EmployeeName, EmployeePhonenumber, EmployeeEmail;
+
+
+        static string message = "Enter the Employee details:";
+        public static void setMessage(string msg)
+        {
+            message = msg;
+        }
+
+
+
+        public static string getMessage()
+        {
+            return message;
+        }
+
+    }
+
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace demo
+{
+    class Program
+    {
+        public static SqlDbType EmployeeEmail { get; private set; }
+        public static SqlDbType EmployeePhonenumber { get; private set; }
+
+        static void Main(string[] args)
+        {
+
+            var employee = new Validatedetails();
+            employee.ValidateEmployeeId();
+            employee.ValidateEmployeeName();
+            employee.ValidateEmployeeEmail();
+            employee.ValidateEmployeePhoneNumber();
+            employee.ValidateDateOfBirth();
+            employee.connectTODb();
+            employee.ValidateEmployeeprintdetails();
+            Console.ReadLine();
+            
+            }
+
+
+        }
+    }
